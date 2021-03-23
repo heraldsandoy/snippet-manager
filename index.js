d3e1ed478bf84af744378fd417a358d4fc267c66
heraldsandoy <sandoyherald1993@gmail.com>
2021-03-23 23:48:29 +0800

    add PORT env var for express listen

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(cors({
   origin: "http://localhost:3000"
 }));
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // set up routers
 
